506ce52ddbf7e2c4e649ae3a8b3b33ae257d5ecf
Vladimir_Kharchenko <RedLongCity@gmail.com>
2017-12-15 22:19:12 +0200

    Changed Global Job

--- src/main/java/com/redlongcitywork/easytourlite/quartz/jobs/GlobalUpdatingJob.java
+++ src/main/java/com/redlongcitywork/easytourlite/quartz/jobs/GlobalUpdatingJob.java
@@ -1,7 +1,10 @@
 package com.redlongcitywork.easytourlite.quartz.jobs;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.redlongcitywork.easytourlite.extractor.HotFiltersExtractor;
 import com.redlongcitywork.easytourlite.quartz.services.QuartzService;
 import com.redlongcitywork.easytourlite.singletons.AppConstants;
+import com.redlongcitywork.easytourlite.utils.HttpUtils;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.quartz.DisallowConcurrentExecution;
@@ -18,11 +21,10 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
  * 10.09.2017
  * class for refreshing filters
  */
-
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
-public class GlobalUpdatingJob extends QuartzJobBean{
-
+public class GlobalUpdatingJob extends QuartzJobBean {
+    
     private static final Logger LOG = Logger.getLogger(GlobalUpdatingJob.class.getName());
     
     @Autowired
@@ -31,20 +33,37 @@ public class GlobalUpdatingJob extends QuartzJobBean{
     @Autowired
     QuartzService quartzService;
     
+    @Autowired
+    HotFiltersExtractor extractor;
+    
     @Override
     protected void executeInternal(JobExecutionContext jec) throws JobExecutionException {
-        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this); 
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
         LOG.log(Level.INFO, "Global Job Doing");
         constants.setGlobalSuspended(false);
+        
+        extractor.extract(new HttpUtils.GetCallBack() {
+            @Override
+            public void onDataReceived(JsonNode node) {
+                LOG.log(Level.INFO, "Hot Tours Filters were extracting successfully");
+            }
+            
+            @Override
+            public void onDataNotAwailable() {
+                LOG.log(Level.WARNING, "Hot Tours extracting was failed!");
+            }
+        });
         //resumeShortUpdateJob(jec);
         //pauseItSelf(jec);
     }
     
-        private void resumeShortUpdateJob(JobExecutionContext jec){
+    private void resumeShortUpdateJob(JobExecutionContext jec) {
         quartzService.resumeShortJob();
     }
-            private void pauseItSelf(JobExecutionContext jec){
-            constants.setGlobalSuspended(true);
-            quartzService.pauseGlobalJob();
+    
+    private void pauseItSelf(JobExecutionContext jec) {
+        constants.setGlobalSuspended(true);
+        quartzService.pauseGlobalJob();
     }
+    
 }
