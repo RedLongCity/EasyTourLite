d992cf9c2a04159ddda81a003b613dfaf1526309
Vladimir_Kharchenko <RedLongCity@gmail.com>
2018-02-28 22:44:15 +0200

    Added SearchParamsExtractor

--- src/main/java/com/redlongcitywork/easytourlite/utils/ItToursUrls.java
+++ src/main/java/com/redlongcitywork/easytourlite/utils/ItToursUrls.java
@@ -4,19 +4,16 @@ package com.redlongcitywork.easytourlite.utils;
  *
  * @author redlongcity
  */
-
 public interface ItToursUrls {
-    
-                    //Base 
-    
+
+    //Base 
     String api_base_url = "http://api.ittour.com.ua/";
     String authorization = "Authorization";
     String authorization_token = "d5b80c6bbb43d945faa9577e80f24a22";
     String acceptLanguage = "Accept-Language";
-    String response_language   = "ru";
-    
-                    //Showcase
-    
+    String response_language = "ru";
+
+    //Showcase
     String api_showcases = "showcase/hot-offers";
     String api_showcases_filters = "/filters";
     String api_showcases_search = "/search?";
@@ -26,9 +23,8 @@ public interface ItToursUrls {
     String api_showcases_page = "&page=";
     String api_showcases_items_per_page = "&items_per_page=100&";
     String api_showcases_hotel_image = "hotel_image=1";
-    
-                    //Others
-    
+
+    //Others
     String api_params_url = "module/params";
     String api_search_url = "module/search";
     String api_search_by_keys = "module/search-list";
@@ -36,7 +32,5 @@ public interface ItToursUrls {
     String api_tour_validate_url = "tour/validate/";
     String api_tour_flights_urs = "tour/flights/";
     String api_tour_info = "/tour/info/";
-    
 
-    
 }
